8212bd75809b90d54b84df20586c6b619b46b39c
gameforcela <46069711+gameforcela@users.noreply.github.com>
2019-01-14 01:08:19 +0100

    zkouška_pohybu

---
+++ pec/Assets/sctipts/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject objekt;
+    public GameObject temParent;
+    public Transform center;
+
+    void Start()
+    {
+        objekt.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnMouseDown()
+    {
+        objekt.GetComponent<Rigidbody>().useGravity = false;
+        objekt.GetComponent<Rigidbody>().isKinematic = true;
+        objekt.transform.position = center.transform.position;
+        objekt.transform.rotation = center.transform.rotation;
+        objekt.transform.parent = temParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+        objekt.GetComponent<Rigidbody>().useGravity = true;
+        objekt.GetComponent<Rigidbody>().isKinematic = false;            
+        objekt.transform.parent = null;
+    }
+}
