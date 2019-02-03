bf7fd3c8489e9e8a821991e927088969d3f82bfb
gameforcela <46069711+gameforcela@users.noreply.github.com>
2019-02-03 13:29:51 +0100

    xxx

---
+++ Kovárna/Assets/interaktion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interaktion : MonoBehaviour
+{
+
+    private bool dOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("ONMOUSE");
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("exit");
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("down");
+       
+    }
+}
