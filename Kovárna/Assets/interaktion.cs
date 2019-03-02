11e8fbe75ef8fe56702517bbc9acaa0adb91bd13
KoniHlava <riedladam@seznam.cz>
2019-03-02 15:42:39 +0100

    Test

---
+++ Kovárna/Assets/interaktion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interaktion : MonoBehaviour
+{
+
+    public GameObject Ore;
+    public GameObject Hero;
+    public bool OreActive = false;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("ONMOUSE");
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("exit");
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("down");
+
+        OreActive = true;
+        if (OreActive == true)
+        {
+            Ore.SetActive(true);
+        } else
+        {
+            Ore.SetActive(false);
+        }       
+    }
+
+}
